a247b4138acc2c1c2a71484ce7a534156a6ec256
AMISHA NARSINGANI <amishanarsingani@gmail.com>
2020-07-11 00:18:40 +0530

    updating list on text input

--- lib/main.dart
+++ lib/main.dart
@@ -6,16 +6,63 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('List View In Flutter'),
         ),
-        body: Center(
-          //child: Text('Hello World!'),
-          child: Text(wordPair.asPascalCase),
-        ),
+        body: ListDisplay(),
+      ),
+    );
+  }
+}
+
+class ListDisplay extends StatefulWidget {
+  @override
+  _ListDisplayState createState() => _ListDisplayState();
+}
+
+class _ListDisplayState extends State<ListDisplay> {
+  List<String> listItem = ['India'];
+  final TextEditingController eCtrl = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        children: <Widget>[
+          TextField(
+            obscureText: false,
+            decoration: InputDecoration(
+                contentPadding: EdgeInsets.all(20.0),
+                border: OutlineInputBorder(
+                  borderRadius: new BorderRadius.circular(25.0),
+                ),
+                labelText: 'Enter a country'),
+            controller: eCtrl,
+            onSubmitted: (text) {
+              listItem.add(text);
+              eCtrl.clear();
+              setState(() {});
+            },
+            keyboardType: TextInputType.text,
+          ),
+          SizedBox(
+            height: 20.0,
+          ),
+          Expanded(
+            child: new ListView.builder(
+                itemCount: listItem.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return new Text(
+                    listItem[index],
+                    style: TextStyle(
+                      fontSize: 20.0,
+                    ),
+                  );
+                }),
+          ),
+        ],
       ),
     );
   }
