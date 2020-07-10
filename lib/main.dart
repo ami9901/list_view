78b7cb72f97b1eedf5a054a4f90ab4d8563cd711
AMISHA NARSINGANI <amishanarsingani@gmail.com>
2020-07-10 20:57:23 +0530

    Displaying random word pair

--- lib/main.dart
+++ lib/main.dart
@@ -6,13 +6,15 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('List View In Flutter'),
         ),
         body: Center(
-          child: Text('Hello World!'),
+          //child: Text('Hello World!'),
+          child: Text(wordPair.asPascalCase),
         ),
       ),
     );
